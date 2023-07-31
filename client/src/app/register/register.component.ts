cb10d0c674cc57c01a254b0231d8999626333ce8
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-30 23:19:25 -0300

    IJid

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  public registroCerrado: boolean = false;
 
+  ngOnInit() {
+    // Fecha objetivo (12 de agosto)
+    const fechaObjetivo = new Date("2023-08-12T00:00:00");
+    const fechaActual = new Date();
+
+    // Si la fecha actual es mayor o igual a la fecha objetivo, se ha pasado la fecha de registro
+    if (fechaActual >= fechaObjetivo) {
+      this.registroCerrado = true;
+    }
+  }
 }
